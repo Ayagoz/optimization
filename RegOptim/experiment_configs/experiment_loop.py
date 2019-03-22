962c668fad20cd15eaa29fc4bac0b278452b6f3f
Ayagoz <ayagoz.musabaeva@mail.ru>
2019-03-22 09:08:57 +0300

    pad size

--- RegOptim/experiment_configs/experiment_loop.py
+++ RegOptim/experiment_configs/experiment_loop.py
@@ -75,7 +75,7 @@ def pipeline_main_loop_template_only(data, template, y, idx_out_train, idx_out_t
 
     kwargs = create_kwargs(pipeline_params, data, template, a, b, idx_out_train, True, add_padding, pad_size)
     y_out_train = y[idx_out_train]
-
+    print(f'before while pad size {pad_size}, add_padding {add_padding}')
     while it < pipeline_params['Number_of_iterations']:
 
         st = time.time()
@@ -123,7 +123,11 @@ def pipeline_main_loop_template_only(data, template, y, idx_out_train, idx_out_t
 
             kwargs['add_padding'] = True
             kwargs['pad_size'] += reg['pad_size']
+            add_padding = True
+            pad_size += reg['pad_size']
 
+        print(f'after check padding pad size {pad_size}, add_padding {add_padding}')
+        print(f'template shape {template.shape}')
         if add_padding:
             pipeline_params['pipeline_optimization_params']['add_padding'] = add_padding
 
@@ -270,6 +274,9 @@ def optimize_template_step(data, template, y, a, b, idx_out_train, idx_out_test,
 
         add_padding = True
         pad_size += reg['pad_size']
+        kwargs['add_padding'] = True
+        kwargs['pad_size'] += reg['pad_size']
+        
 
     gc.collect()
     return template, best_params, grads_da, grads_db, train_score, test_score, train_loss, test_loss, add_padding, pad_size
